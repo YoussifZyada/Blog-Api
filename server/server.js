1cf6fa9d6213a65a3c2537cff956db868420a9d9
YoussifZyada <youssifzyd1@gmail.com>
2026-04-06 01:38:38 +0200

    added testRoute line to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import testRoutes from "./routes/testRoutes.js";
+
 
 dotenv.config();
 connectDB();
@@ -9,9 +11,8 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
-
+app.use("/api/test", testRoutes);
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
